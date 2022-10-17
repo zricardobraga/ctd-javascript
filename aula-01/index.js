573a923e4c6b3d209db7607b64c24327b8faf386
zricardobraga <zericardobraga@gmail.com>
2022-10-17 20:58:21 -0300

    anotações sobre pensamento computacional

---
+++ aula-01/index.js
@@ -0,0 +1,13 @@
+/*
+
+Decompor: quebrar um sistema ou um problema complexo em partes menores;
+
+Detectar padrões: observar padrões que se repetem e otimizar isso;
+
+Abstrair: analisar caracteristicas do problema e focar apenas no que é importante, deixando de lado o que é irrelevante;
+
+Algoritmo: instruções passo a passo ou estabelecer regras a serem seguidas para resolver cada um desses problemas.
+
+Importante: as etapas do pensamento computacional não são sequenciais. 
+
+*/
